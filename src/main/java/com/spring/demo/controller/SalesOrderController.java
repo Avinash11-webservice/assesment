d6fb3ce80286e354a586fdaf727e7892cea44e97
Avinash kumar Anand <avinash.anand@espire.com>
2021-10-06 21:25:07 +0530

    Ecommerse project

---
+++ src/main/java/com/spring/demo/controller/SalesOrderController.java
@@ -0,0 +1,11 @@
+package com.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Avinash
+ *
+ */
+@RestController
+public class SalesOrderController {
+}
